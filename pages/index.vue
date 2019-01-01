2665e6924ec410b2bdf08c039fea57126c3a74a6
yuta0801 <yuta20030801@icloud.com>
2019-01-02 02:33:27 +0900

    [追加]フォームを追加

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,13 @@
 <template>
   <div>
     <h1>bbs</h1>
+    <form 
+      action="/" 
+      method="post">
+      <input 
+        type="text" 
+        name="message">
+      <input type="submit">
+    </form>
   </div>
 </template>
