2b556106a185ff35615f038b1ebe1583ffa1e6a7
Yusuke Hayashi <yusuke8h@gmail.com>
2019-01-03 22:25:26 +0900

    [修正]nodeをmake install

--- pipeline/install.sh
+++ pipeline/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/ec2-user/repos/bbs
-/home/ec2-user/.nvm/versions/node/v11.6.0/bin/npm install
-/home/ec2-user/.nvm/versions/node/v11.6.0/bin/npm run build
+npm install
+npm run build
