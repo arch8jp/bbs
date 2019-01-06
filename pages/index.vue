234b64ad38113d39a127b819e84f3725da9fba11
yuta0801 <yuta20030801@icloud.com>
2019-01-06 11:30:24 +0900

    投稿コンポーネントを作成してサンプルを表示

--- pages/index.vue
+++ pages/index.vue
@@ -9,5 +9,18 @@
         name="message">
       <input type="submit">
     </form>
+    <post content="hoge" />
+    <post content="fuga" />
+    <post content="piyo" />
   </div>
 </template>
+
+<script>
+import Post from '~/components/Post.vue'
+
+export default {
+  components: {
+    Post
+  }
+}
+</script>
