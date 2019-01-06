8586af3a06541fa44b475825491b3513e25027ae
Yusuke Hayashi <yusuke8h@gmail.com>
2019-01-06 14:06:36 +0900

    SQLite3にデータを永続化

--- pages/index.vue
+++ pages/index.vue
@@ -2,44 +2,29 @@
   <div>
     <h1>bbs</h1>
     <form @submit.prevent="onSubmit">
-      <input
-        v-model="message"
-        type="text"
-        required>
+      <input v-model="message" type="text" required>
       <input type="submit">
     </form>
-    <post
-      v-for="(message, i) in messages"
-      :content="message"
-      :key="i" />
+    <post v-for="(message, i) in messages" :content="message" :key="i"/>
   </div>
 </template>
 
 <script>
-import Post from '~/components/Post.vue'
+import Post from "~/components/Post.vue";
 
 export default {
-  components: {
-    Post
-  },
+  components: { Post },
   async asyncData({ res }) {
-    return {
-      messages: res.data.messages
-    }
+    return { messages: res.data.messages };
   },
   data() {
-    return {
-      message: '',
-      messages: []
-    }
+    return { message: "", messages: [] };
   },
   methods: {
     onSubmit() {
-      this.$axios.$post('/', {
-        message: this.message
-      })
-      this.message = ''
+      this.$axios.$post("/", { message: this.message });
+      this.message = "";
     }
   }
-}
+};
 </script>
--- server/index.js
+++ server/index.js
@@ -1,24 +1,44 @@
+const util = require("util");
 const express = require("express");
 const bodyParser = require("body-parser");
 const consola = require("consola");
+const sqlite3 = require("sqlite3").verbose();
 const { Nuxt, Builder } = require("nuxt");
 const app = express();
 const host = process.env.HOST || "127.0.0.1";
 const port = process.env.PORT || 3000;
 
-const posts = [];
+const db = new sqlite3.Database("bbs.db");
+db.run(
+  `CREATE TABLE IF NOT EXISTS posts(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  message STRING,
+  user STIRNG)`
+);
+const dbAll = util.promisify((sql, arg, callback) =>
+  db.all(sql, arg, callback)
+);
+const dbRun = util.promisify((sql, arg, callback) =>
+  db.run(sql, arg, callback)
+);
+const addPost = message =>
+  dbRun("INSERT INTO posts(message) VALUES(?)", message);
+const getPosts = () => dbAll("SELECT id, message FROM posts LIMIT 10");
+const filterMessages = posts => posts.map(post => post.message);
 
 app.set("port", port);
 
 app.use(bodyParser.json());
 
-app.get("/", (req, res, next) => {
-  res.data = { messages: posts };
+app.get("/", async (req, res, next) => {
+  const posts = await getPosts();
+  const messages = filterMessages(posts);
+  res.data = { messages };
   next();
 });
 
 app.post("/", (req, res) => {
-  posts.push(req.body.message);
+  addPost(req.body.message);
   res.end();
 });
 
