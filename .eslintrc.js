691e91925504dd66b672c0a99992871e51a7f4a2
Yusuke Hayashi <yusuke8h@gmail.com>
2019-01-06 13:21:56 +0900

    eslint and prettierの設定

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,25 @@
 module.exports = {
   root: true,
   env: {
+    es6: true,
     browser: true,
     node: true
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: "babel-eslint"
   },
   extends: [
-    'plugin:vue/recommended',
-    'plugin:prettier/recommended'
+    "plugin:vue/recommended",
+    "problems",
+    "plugin:vue/strongly-recommended",
+    "plugin:prettier/recommended"
   ],
   // required to lint *.vue files
-  plugins: [
-    'vue',
-    'prettier'
-  ],
+  plugins: ["vue", "prettier"],
   // add your custom rules here
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    "prettier/prettier": "error",
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
   }
-}
+};
--- server/index.js
+++ server/index.js
@@ -1,49 +1,49 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const consola = require('consola')
-const { Nuxt, Builder } = require('nuxt')
-const app = express()
-const host = process.env.HOST || '127.0.0.1'
-const port = process.env.PORT || 3000
+const express = require("express");
+const bodyParser = require("body-parser");
+const consola = require("consola");
+const { Nuxt, Builder } = require("nuxt");
+const app = express();
+const host = process.env.HOST || "127.0.0.1";
+const port = process.env.PORT || 3000;
 
-const posts = []
+const posts = [];
 
-app.set('port', port)
+app.set("port", port);
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.get('/', (req, res, next) => {
-  res.data = { messages: posts }
-  next()
-})
+app.get("/", (req, res, next) => {
+  res.data = { messages: posts };
+  next();
+});
 
-app.post('/', (req, res) => {
-  posts.push(req.body.message)
-  res.end()
-})
+app.post("/", (req, res) => {
+  posts.push(req.body.message);
+  res.end();
+});
 
 // Import and Set Nuxt.js options
-let config = require('../nuxt.config.js')
-config.dev = !(process.env.NODE_ENV === 'production')
+const config = require("../nuxt.config.js");
+config.dev = !(process.env.NODE_ENV === "production");
 
 async function start() {
   // Init Nuxt.js
-  const nuxt = new Nuxt(config)
+  const nuxt = new Nuxt(config);
 
   // Build only in dev mode
   if (config.dev) {
-    const builder = new Builder(nuxt)
-    await builder.build()
+    const builder = new Builder(nuxt);
+    await builder.build();
   }
 
   // Give nuxt middleware to express
-  app.use(nuxt.render)
+  app.use(nuxt.render);
 
   // Listen the server
-  app.listen(port, host)
+  app.listen(port, host);
   consola.ready({
     message: `Server listening on http://${host}:${port}`,
     badge: true
-  })
+  });
 }
-start()
+start();
