21348f895375497092ef8e87e5a0ea408848ea24
yuta0801 <yuta20030801@icloud.com>
2019-01-09 15:22:49 +0900

    Vuetifyを使う

--- components/Post.vue
+++ components/Post.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
-    <p>{{ content }}</p>
-    <hr>
+    <v-list-tile>
+      <v-list-tile-content>
+        <p>{{ content }}</p>
+      </v-list-tile-content>
+    </v-list-tile>
+    <v-divider/>
   </div>
 </template>
 
